580490a2f50dc1a9ec8ef00180c13057888ca1fe
Alan Jahir <alan.martinez5691@alumnos.udg.mx>
2022-09-13 12:47:25 -0500

    Logout y mantener sesion activa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,11 +13,24 @@
                 <h1 class="text-3xl font-black">
                     DevStagram
                 </h1>
-
+                    
+                @auth
                 <nav class="flex gap-2 items-center">
-                    <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
-                    <a href="{{ route('register') }}" class="font-bold uppercase text-gray-600 text-sm">Crear cuenta</a>
+                    <a class="font-bold text-gray-600 text-sm" href="#">Hola: <span class="font-normal">{{ auth()->user()->username }} </span></a>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit" class="font-bold uppercase text-gray-600 text-sm">Cerrar sesión</button>
+                    </form>
                 </nav>
+                @endauth
+
+                @guest
+                    <nav class="flex gap-2 items-center">
+                        <a class="font-bold uppercase text-gray-600 text-sm" href="#">Login</a>
+                        <a href="{{ route('register') }}" class="font-bold uppercase text-gray-600 text-sm">Crear cuenta</a>
+                    </nav>
+                @endguest
+
             </div>
         </header>
 
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
 
 /*
@@ -25,6 +26,7 @@ Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('login', [LoginController::class, 'index'])->name('login');
 Route::post('login', [LoginController::class, 'store']);
+Route::post('logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/muro', [PostController::class, 'index'])->name('post.index');
 
