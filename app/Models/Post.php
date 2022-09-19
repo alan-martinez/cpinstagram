cfb58b855f59f30102399cbc60aff475e8fa1c56
Alan Jahir <alan.martinez5691@alumnos.udg.mx>
2022-09-19 11:58:08 -0500

    Likes a las publicaciones

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Models\Like;
+use App\Models\User;
 use App\Models\Comentario;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -24,5 +26,17 @@ class Post extends Model
     public function comentarios()
     {
         return $this->hasMany(Comentario::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    public function checkLike(User $user)
+    {
+        //Se posiciona en la tabla de likes
+        //Usando contains -> contiene en la columna de user_id este usuario de este post
+        return $this->likes->contains('user_id', $user->id);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
-use App\Http\Controllers\ComentarioController;
-use App\Http\Controllers\ImagenController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LikeController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ComentarioController;
 
 /*
 |--------------------------------------------------------------------------
@@ -41,3 +42,7 @@ Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.
 Route::post('/{user:username}/posts/{post}', [ComentarioController::class, 'store'])->name('comentarios.store');
 
 Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.store');
+
+//Likes a las fotos 
+Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
+Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
